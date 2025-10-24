4875b2486fb883a2b77ed040a4fa4b957eb59a2d
framirezj <12649259+framirezj@users.noreply.github.com>
2025-10-24 16:14:47 -0300

    config: instalación dependencias y estructura de carpetas

---
+++ frontend/marketplace/routes/AppRouter.jsx
@@ -0,0 +1,15 @@
+export default function AppRouter() {
+  return (
+    <h1 class="text-3xl font-bold underline bg-gray-500">Hello world!</h1>
+    /*
+        <Routes>
+         <Route path="/" element={<Navigate to="/home" />} />
+
+             <Route element={<LayoutMain />}>
+                 <Route path="/home" element={<Home />} />
+                 <Route path="/paciente" element={<Paciente />} />
+             </Route>
+         </Routes>
+        */
+  );
+}
