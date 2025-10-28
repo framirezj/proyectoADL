28af6ae5f61e84c323c1335fd94dfced7753b490
framirezj <12649259+framirezj@users.noreply.github.com>
2025-10-28 10:58:36 -0300

    feat(navbar): muestra botones de miperfil y mispublicaciones solo si esta autenticado

--- frontend/marketplace/components/layout/Header.jsx
+++ frontend/marketplace/components/layout/Header.jsx
@@ -1,18 +1,15 @@
-import { Link } from 'react-router-dom'
-import { useAuth } from '../../src/context/AuthContext';
-import { useNavigate } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import { useAuth } from "../../src/context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-
-  const { logout, isAuthenticated } = useAuth()
-  const navigate = useNavigate()
-
+  const { logout, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    logout()
-    navigate('/login')
-  }
+    logout();
+    navigate("/login");
+  };
 
   return (
     <header className="navbar bg-base-100 flex justify-evenly shadow-sm">
@@ -25,16 +22,30 @@ export default function Header() {
         <a className="btn btn-ghost text-xl">Pulga Vibe</a>
       </div>
       <div className="navbar-end flex gap-4">
-        <Link to="/" className='btn'>Inicio</Link>
-        <Link to="#" className='btn'>Productos</Link>
-        <Link to="/mispublicaciones" className='btn'>Mis Publicaciones</Link>
-        <Link to="/miperfil" className='btn'>Mi Perfil</Link>
+        <Link to="/" className="btn">
+          Inicio
+        </Link>
+        <Link to="#" className="btn">
+          Productos
+        </Link>
 
-        {!isAuthenticated ? 
-        <Link to="/login" className='btn'>Inicio Sesión</Link>
-        :
-        <button onClick={handleLogout} className='btn'>Cerrar Sesión</button>  
-      }
+        {!isAuthenticated ? (
+          <Link to="/login" className="btn">
+            Inicio Sesión
+          </Link>
+        ) : (
+          <>
+            <Link to="/mispublicaciones" className="btn">
+              Mis Publicaciones
+            </Link>
+            <Link to="/miperfil" className="btn">
+              Mi Perfil
+            </Link>
+            <button onClick={handleLogout} className="btn">
+              Cerrar Sesión
+            </button>
+          </>
+        )}
       </div>
     </header>
   );
